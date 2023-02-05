922b7abb61f99853544eecac0e269bca360d954c
Arseniy1123 <arseniytushin@mail.ru>
2023-02-05 22:38:19 +0300

    SQL-interpreter
    SQL-interpreter

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "lex.h"
+#include "syntax.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    try{
+        int y = 0;
+        cout << "What part do you want to test? (1 - lexical, 2 - syntax)" << endl;
+        cin >> y;
+        if (y == 1) {
+            Scanner scan("mytest.txt");
+            Lex H = scan.get_lex();
+            cout << H << endl;
+            while(H.get_type() != LEX_EXIT){
+                H = scan.get_lex();
+                cout << H << endl;
+            }       
+        }
+        else if (y == 2) {
+            Parser Par("mytest.txt");
+            Par.analyze();
+            Par.poliz.print();
+        }           
+    	return 0;
+    }
+    catch(char c){
+        cout << "\nunexpected symbol " << c << endl;
+        return 1;
+    }
+    catch(Lex l){
+        cout << "\nunexpected lexeme " << l << endl;
+        return 1;
+    }
+    catch(const char *source){
+        cout << source << endl;
+        return 1;
+    }
+    catch(...){
+        cout << "\nUnknown error" << endl;
+        return 1;
+    }
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,11 @@
+PROG = compiler
+FLAGS = -g -Wall -fsanitize=address -fno-omit-frame-pointer
+CC = g++
+
+$(PROG): main.cpp lex.o poliz.o syntax.o 
+	$(CC) $(FLAGS) -c lex.cpp -o lex.o
+	$(CC) $(FLAGS) -c poliz.cpp -o poliz.o
+	$(CC) $(FLAGS) -c syntax.cpp -o syntax.o
+	$(CC) $(FLAGS) main.cpp lex.o poliz.o syntax.o -o $(PROG)
+clean:
+	rm -f *.o $(PROG)
